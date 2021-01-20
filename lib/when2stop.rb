979df29e8a509e7268a9215bff729459cfd4d7e0
Pablo Cha <pabloc@fulljaus.com>
2021-01-20 08:48:31 -0300

    V 1.0.0 - Add Plugin logic

--- lib/when2stop.rb
+++ lib/when2stop.rb
@@ -1,5 +1,32 @@
 require "when2stop/railtie"
 
 module When2stop
-  # Your code goes here...
+  require 'when2stop/timer'
+
+  def iterate options = { for: 10.minutes }, &block
+    timer = Timer.new for: options[:for]
+
+    enum = options[:enum] || false
+    if enum
+      enum.each do |el|
+        if timer.there_is_more_time?
+          ret = block.call el
+          if options.has_key? :break_when_returns and ret == options[:break_when_returns]
+            break
+          end
+        else
+          break
+        end
+      end
+    else
+      begin
+        ret = block.call
+        if options.has_key? :break_when_returns and ret == options[:break_when_returns]
+          break
+        end
+      end while timer.there_is_more_time?
+    end
+
+    timer.get_elapsed_time
+  end
 end
